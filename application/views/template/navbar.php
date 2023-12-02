5f9ec92627984e0f478e8715894db926ecb9459a
Yogi Prasetio <yogiprasetio25@gmail.com>
2023-12-02 20:22:21 +0700

    remove colour configuration

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -163,7 +163,7 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                    <!-- <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
                         <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
                             <div class="sidenav-toggler-inner">
                                 <i class="sidenav-toggler-line bg-white"></i>
@@ -174,7 +174,7 @@
                         <a href="javascript:;" class="nav-link text-white p-0">
                             <i class="fa fa-cog fixed-plugin-button-nav cursor-pointer"></i>
                         </a>
-                    </li>
+                    </li> -->
                     
                 </ul>
             </div>
